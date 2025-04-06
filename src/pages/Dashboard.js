265a8bd868d6b5563ba774208ff2e6892cbbfefe
Sally E <sme418@nyu.edu>
2025-04-06 19:27:20 -0400

    fixed nav bar for mobile

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,63 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../components/Header';
-import { useNavigate } from 'react-router-dom';
-import TextPost from '../components/Post';
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
+import TextPost from "../components/Post";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 const queryData = async (app) => {
-    if (!app) return [];
-    const db = getFirestore(app);
-    const querySnapshot = await getDocs(collection(db, "posts"));
-    const data = [];
-    querySnapshot.forEach((doc) => {
-        data.push(doc.data());
-    });
-    return data;
+  if (!app) return [];
+  const db = getFirestore(app);
+  const querySnapshot = await getDocs(collection(db, "posts"));
+  const data = [];
+  querySnapshot.forEach((doc) => {
+    data.push(doc.data());
+  });
+  return data;
 };
 
-function Dashboard({ 
-    app,
-    isLoading, 
-    isLoggedIn, 
-    setIsLoggedIn, 
-    setUserInformation, 
-    userInformation 
-}){
-    const navigate = useNavigate();
-    const [postData, setPostData] = useState([]);
+function Dashboard({
+  app,
+  isLoading,
+  isLoggedIn,
+  setIsLoggedIn,
+  setUserInformation,
+  userInformation,
+}) {
+  const navigate = useNavigate();
+  const [postData, setPostData] = useState([]);
 
-    // if not logged in & not loading navigate to login page
-    useEffect(() => {
-        if(!isLoggedIn && !isLoading) navigate("/login");
-    }, [isLoading, isLoggedIn, navigate]);
+  // if not logged in & not loading navigate to login page
+  useEffect(() => {
+    if (!isLoggedIn && !isLoading) navigate("/login");
+  }, [isLoading, isLoggedIn, navigate]);
 
-    useEffect(() => {
-        if(!app) return;
-        queryData(app).then(setPostData);
-    }, [app]);
+  useEffect(() => {
+    if (!app) return;
+    queryData(app).then(setPostData);
+  }, [app]);
 
-    return (
-        <>
-            <Header 
-                isLoggedIn={isLoggedIn}
-                setIsLoggedIn={setIsLoggedIn} 
-                setUserInformation={setUserInformation}
+  return (
+    <>
+      <Header
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+        setUserInformation={setUserInformation}
+      />
+      <div className="PageWrapper">
+        <h1 className="PageTitle">Activity Feed</h1>
+        <div className="PostWrapper">
+          {postData.map((post) => (
+            <TextPost
+              content={post.content}
+              feeling={post.feeling}
+              userId={post.userId}
+              userName={post.userName}
             />
-            <div className="PageWrapper">
-                <h1 className='PageTitle'>Activity Feed</h1>
-                <div className="PostWrapper">
-                    { postData.map((post) => (
-                        <TextPost 
-                            content={post.content}
-                            feeling={post.feeling}
-                            userId={post.userId}
-                            userName={post.userName}
-                        />  
-                    ))}
-                </div>  
-            </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
